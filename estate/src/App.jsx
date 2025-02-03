f355b653303dd93ff6392057d089552e17714636
Husain <husaincementwalas@gmail.com>
2025-02-03 23:32:53 +0530

    about page

--- estate/src/App.jsx
+++ estate/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import Header from './components/Header'
+import About from './components/About'
 
 const App = () => {
   return (
-    <div className=''>
+    <div className='w-full overflow-hidden'>
   
       <Header/>
+      <About/>
     </div>
   )
 }
